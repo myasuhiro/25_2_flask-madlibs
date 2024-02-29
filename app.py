e90894e5f3118b5bda82c867f01ff4676bb1e3f6
yasuhiro.m <yasuhiro@m.com>
2024-02-28 19:18:11 -0800

    modify

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def asking():
+    """Form to ask words"""
+    prompts = story.prompts
+
+    return render_template('asking.html', prompts=prompts)
+
+@app.route('/story')
+def show_story():
+    """Show result"""
+    text = story.generate(request.args)
+
+    return render_template('story.html', text=text)
